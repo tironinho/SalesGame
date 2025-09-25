83f446526167b54e4fb2d473f8d059f9c79691da
tironinho <tironinho@hotmail.com>
2025-09-25 12:56:54 -0300

    terceiro ajuste com peoes funcionando

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -127,7 +127,7 @@ export default function Board({
       {/* ====== MODO NORMAL ====== */}
       {!recordTrack && (
         <>
-          <BoardMarkers visible={true} boardWidth={size.w} boardHeight={size.h} />
+          <BoardMarkers visible={false} boardWidth={size.w} boardHeight={size.h} />
 
           {players.map((p, idx) => {
             const isTurn = idx === turnIdx
